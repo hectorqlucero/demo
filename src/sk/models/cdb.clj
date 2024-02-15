75e9b3316669fe72daa9595209cd57167837216f
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2024-02-15 08:26:32 -0800

    added users data

---
+++ src/sk/models/cdb.clj
@@ -0,0 +1,39 @@
+(ns sk.models.cdb
+  (:require [noir.util.crypt :as crypt]
+            [sk.models.crud :refer [db Insert-multi Query!]]))
+
+(def users-rows
+  [{:lastname  "User"
+    :firstname "Regular"
+    :username  "user@gmail.com"
+    :password  (crypt/encrypt "user")
+    :dob       "1957-02-07"
+    :email     "user@gmail.com"
+    :level     "U"
+    :active    "T"}
+   {:lastname "User"
+    :firstname "Admin"
+    :username "admin@gmail.com"
+    :password (crypt/encrypt "admin")
+    :dob "1957-02-07"
+    :email "admin@gmail.com"
+    :level "A"
+    :active "T"}
+   {:lastname "User"
+    :firstname "System"
+    :username "sistema@gmail.com"
+    :password (crypt/encrypt "sistema")
+    :dob "1957-02-07"
+    :email "sistema@gmail.com"
+    :level "S"
+    :active "T"}])
+
+(defn populate-tables
+  "Populates table with default data"
+  [table rows]
+  (Query! db (str "LOCK TABLES " table "WRITE;"))
+  (Insert-multi db (keyword table) rows)
+  (Query! db "UNLOCK TABLES;"))
+
+(defn database []
+  (populate-tables "users" users-rows))
