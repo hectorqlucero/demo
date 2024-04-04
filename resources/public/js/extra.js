dfa981f573264cbd4e18ad790ab65536d46b4a07
Hector Lucero <hectorqlucero@gmail.com>
2024-04-03 22:08:59 -0700

    Fixed delete confirmation

--- resources/public/js/extra.js
+++ resources/public/js/extra.js
@@ -1,11 +1,13 @@
 /* Display confirmation on clicking a delete button with the value 'Delete' */
-$('.confirm').each(function(){
-  var href = $(this).attr('href');
-  $(this).click(function(){
-    if(confirm("Esta usted seguro?")){
-      window.location.href = href;
-    } else {
-      return false;
-    }
+$(document).ready(function(){
+  $('.confirm').each(function(){
+    var href = $(this).attr('href');
+    $(this).click(function(){
+      if(confirm("Esta usted seguro?")){
+        window.location.href = href;
+      } else {
+        return false;
+      }
+    });
   });
 });
